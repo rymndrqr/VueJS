7e09f471f634fe3c4f49196c24cebbb9f0e9ea2f
rymndrqr <raymundoroquero11@gmail.com>
2025-08-14 13:58:27 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,36 @@
-<script setup></script>
 <template>
-  <button class="border-10">Click Me</button>
+  <div>
+    <h1 class="p-2 flex bg-blue-200">User Profiles</h1>
+    <div style="display: flex; flex-wrap: wrap;">
+      <UserCard
+        v-for="(user, index) in users"
+        :key="index"
+        :name="user.name"
+        :age="user.age"
+        :isActive="user.isActive"
+        :hobby="user.hobby"
+      />
+    </div>
+  </div>  
 </template>
+
+<script>
+import UserCard from './components/userCard.vue';
+
+export default {
+  name: "App",
+  components: {
+    UserCard
+  },
+  data() {
+    return {
+      users: [
+        { name: 'Leonard Lopez', age: 20, isActive: true, hobby: 'Watching Movie' },
+        { name: 'Dwight Yana', age: 21, isActive: false, hobby: 'Skateboarding' },
+        { name: 'Jarl Sabillano', age: 29, isActive: true, hobby: 'Basketball' },
+        { name: 'Jay Macantan', age: 28, isActive: false, hobby: 'Eating' }
+      ]
+    };
+  }
+};
+</script>
